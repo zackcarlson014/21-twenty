ae29f47ba1c1a793562f9328b05c1748459d8a61
zackcarlson014 <jzcarlson014@gmail.com>
2023-08-04 20:29:44 -0500

    app js linting

--- src/App.js
+++ src/App.js
@@ -13,10 +13,20 @@ function App({ signOut }) {
   return (
     <View className="App">
       <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
+        <Image
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        
+        <Heading level={1}>
+          We now have Auth!
+        </Heading>
       </Card>
-      <Button onClick={signOut}>Sign Out</Button>
+
+      <Button onClick={signOut}>
+        Sign Out
+      </Button>
     </View>
   );
 }
